8a49c7459c165474505ea56a5de3a76c83c71c43
Ali Fakoor <fakoor@gmail.com>
2024-10-14 22:32:58 -0700

    Enrich hardware specification table Checkpoint for bigger changes

--- BruteForceMnemonic/Helper.cu
+++ BruteForceMnemonic/Helper.cu
@@ -48,15 +48,24 @@ inline int _ConvertSMVer2Cores(int major, int minor)
 
 	sSMtoCores nGpuArchCoresPerSM[] =
 	{
-		{ 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
-		{ 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
-		{ 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
-		{ 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
-		{ 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
-		{ 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
-		{ 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
-		{ 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
-		{   -1, -1 }
+		{ 0x10,  8  }, // Tesla Generation (SM 1.0) G80 class
+		{ 0x11,  8  }, // Tesla Generation (SM 1.1) G8x class
+		{ 0x12,  8  }, // Tesla Generation (SM 1.2) G9x class
+		{ 0x13,  8  }, // Tesla Generation (SM 1.3) GT200 class
+		{ 0x20, 32  }, // Fermi Generation (SM 2.0) GF100 class
+		{ 0x21, 48  }, // Fermi Generation (SM 2.1) GF10x class
+		{ 0x30, 192 }, // Kepler Generation (SM 3.0) GK10x class
+		{ 0x35, 192 }, // Kepler Generation (SM 3.5) GK11x class
+		{ 0x40, 128 }, // Maxwell Generation (SM 5.0) GM10x class
+		{ 0x50, 128 }, // Maxwell Generation (SM 5.2) GM20x class
+		{ 0x60, 64  }, // Pascal Generation (SM 6.0) GP100 class
+		{ 0x61, 128 }, // Pascal Generation (SM 6.1) GP10x class
+		{ 0x62, 128 }, // Pascal Generation (SM 6.2) GP10x class
+		{ 0x70, 64  }, // Volta Generation (SM 7.0) GV100 class
+		{ 0x72, 64  }, // Turing Generation (SM 7.5) TU10x class
+		{ 0x80, 128 }, // Ampere Generation (SM 8.0) GA10x class
+		{ 0x86, 128 }, // Ampere Generation (SM 8.6) GA10x class
+		{ -1, -1 }    // Sentinel value
 	};
 
 	int index = 0;
@@ -100,6 +109,15 @@ int devicesInfo(void)
 		cudaGetDeviceProperties(&deviceProp, dev);
 
 		printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
+
+		size_t freeMem, totalMem;
+		cudaError_t cuErr = cudaMemGetInfo(&freeMem, &totalMem);
+		if (cuErr == cudaSuccess) {
+			printf("Memory: %ul out of %ul is free.\r\n", freeMem / 1024.0 / 1024.0 , totalMem / 1024.0 / 1024.0);
+		}
+		else {
+			printf("Cannot query free memory");
+		}
 
 		// Console log
 		cudaDriverGetVersion(&driverVersion);
